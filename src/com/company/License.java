5743defeabe4cc2d086e96fac0748b54148d0ad4
Cyryl Bulbum <killertom1997@gmail.com>
2017-06-01 10:24:24 +0300

    First commit. Multilicensing appeared. So, there are three licenses now.

---
+++ src/com/company/License.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+class License extends JFrame {
+
+    JTextArea licenseText = new JTextArea();
+    String arrayOfLicenses[] ={"e5f6d5d1e73b288e62f5975db3e1f50e", "f57e35bb561a888513bd5926a10016dc", "b1f344ebc49f50d64644177083936340"};
+    String arrayOfUsernames[] = {"Denis Zasypkin","Anastasia Navros","Andrew Komissarov"};
+
+    public boolean isValid=false;
+    License() throws IOException {
+        JLabel labelLicense = new JLabel("license");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src//com//company//res//license-bg.jpg")))));
+        this.setBounds(45 * Toolkit.getDefaultToolkit().getScreenSize().width / 100, 40 * Toolkit.getDefaultToolkit().getScreenSize().height / 100,  340, 150);
+        this.setForeground(new Color(201, 149, 255));
+        licenseText.setForeground(new Color(173, 183, 202));
+        licenseText.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 10));
+
+
+        labelLicense.setForeground(new Color(196, 202, 198));
+        labelLicense.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 10));
+
+
+
+        this.add(labelLicense).setBounds(130,120,50,20);
+        this.add(licenseText).setBounds(30,100,280,20);
+        this.setUndecorated(true);
+
+        this.setFocusable(true);
+        this.setVisible(true);
+    }
+    void toClose(){
+
+        this.setVisible(false);
+    }
+}
---
+++ src/com/company/MainFrame.java
@@ -0,0 +1,418 @@
+package com.company;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class MainFrame extends JFrame {
+    private int width = 500;
+    private int height = 300;
+
+    private JTextArea login = new JTextArea();
+    private JPasswordField password = new JPasswordField();
+
+    private String correctPassword;
+    private String correctLogin;
+    private String username;
+
+    private JLabel logLabel = new JLabel("login");
+    private JLabel passLabel= new JLabel("password");
+    private JLabel logo = new JLabel();
+
+    private License license;
+
+    private JButton encrypt;
+    private JButton setPassword;
+
+    private boolean isTakenAccess=false;
+    private boolean newPasswordTyped=false;
+
+    MainFrame() throws IOException {
+
+         settings();
+
+        if (toCheckLicense()) this.setVisible(true);
+    }
+
+    private void settings() throws IOException {
+        this.setVisible(false);
+
+        toReadPassword();
+
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src//com//company//res//bg.jpg")))));
+        this.setBounds(40 * Toolkit.getDefaultToolkit().getScreenSize().width / 100, 30 * Toolkit.getDefaultToolkit().getScreenSize().height / 100,  width, height);
+        this.setLayout(null);
+
+
+        login.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        login.setLineWrap(true);
+        login.setWrapStyleWord(true);
+        login.setOpaque(true);
+
+
+        login.setForeground(new Color(43, 33, 202));
+        login.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 12));
+
+
+        password.setForeground(new Color(132, 125, 202));
+        password.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+
+        logLabel.setForeground(new Color(196, 202, 198));
+        logLabel.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 12));
+
+        passLabel.setForeground(new Color(196, 202, 198));
+        passLabel.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 12));
+
+
+        ImageIcon icon3 = new ImageIcon("src//com//company//res//encrypt.jpg");
+        encrypt = new JButton(icon3);
+        encrypt.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (!encrypt.isOpaque()) {
+                    encrypt.setOpaque(true);
+                    encrypt.setBounds(encrypt.getX(), encrypt.getY() + 2, encrypt.getWidth(), encrypt.getHeight());
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (encrypt.isOpaque()) {
+                    encrypt.setOpaque(false);
+                    encrypt.setBounds(encrypt.getX(), encrypt.getY() - 2, encrypt.getWidth(), encrypt.getHeight());
+                if (isTakenAccess){
+                    try {toOutputAndEncryptFile();} catch (IOException e1) {e1.printStackTrace();}
+
+                    try{
+                        Process r = Runtime.getRuntime().exec("cmd /c  del src\\com\\company\\res\\decrypted.txt");
+                        }
+                        catch(Exception e1){
+                            System.out.println(e1.toString());
+                            e1.printStackTrace();
+                        }
+                }
+                    System.exit(0);
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+
+        ImageIcon icon4 = new ImageIcon("src//com//company//res//sets.jpg");
+        setPassword = new JButton(icon4);
+        setPassword.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (!setPassword.isOpaque()) {
+                    setPassword.setOpaque(true);
+                    setPassword.setBounds(setPassword.getX(), setPassword.getY() + 2, setPassword.getWidth(), setPassword.getHeight());
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (setPassword.isOpaque()) {
+                    setPassword.setOpaque(false);
+                    setPassword.setBounds(setPassword.getX(), setPassword.getY() - 2, setPassword.getWidth(), setPassword.getHeight());
+
+                    if (!newPasswordTyped && isTakenAccess) {
+                            passLabel.setText("input new password");
+                            password.setText("");
+                            newPasswordTyped = !newPasswordTyped;
+                            repaint();
+                            return;
+                    }
+
+                    if (newPasswordTyped && isTakenAccess)
+                    {
+                        newPasswordTyped = !newPasswordTyped;
+                        correctPassword =  new String(password.getPassword());//.getPassword().toString();
+
+                        passLabel.setText("password");
+                        try {toChangePassword();} catch (IOException e1) {e1.printStackTrace();}
+                    }
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        logo.setForeground(new Color(196, 202, 198));
+        logo.setFont(new Font("Microsoft JhengHei Light", Font.BOLD, 10));
+
+        DocumentListener listener = new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                if (login.getText().equals(correctLogin) && isPasswordCorrect(password.getPassword()))  try {
+                    toTakeAccess();} catch (InterruptedException e1) {e1.printStackTrace();} catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                if (login.getText().equals(correctLogin) && isPasswordCorrect(password.getPassword()))  try {
+                    toTakeAccess();} catch (InterruptedException e1) {e1.printStackTrace();} catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                if (login.getText().equals(correctLogin) && isPasswordCorrect(password.getPassword()))  try {
+                    toTakeAccess();} catch (InterruptedException e1) {e1.printStackTrace();} catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        };
+
+        password.getDocument().addDocumentListener(listener);
+        login.getDocument().addDocumentListener(listener);
+
+        this.add(login).setBounds(40,100,400,20);
+        this.add(password).setBounds(40,160,400,20);
+        this.add(logLabel).setBounds(40,120,400,20);
+        this.add(passLabel).setBounds(40,180,400,20);
+        this.add(logo).setBounds(180,250,340,20);
+
+        this.setResizable(false);
+    }
+    private boolean isPasswordCorrect(char[] input){
+        if (input.length != correctPassword.length()) return false;
+
+        return Arrays.equals(input, this.correctPassword.toCharArray());
+    }
+    private void toTakeAccess() throws InterruptedException, IOException {
+       login.setForeground(new Color(85, 255, 133));
+       password.setForeground(new Color(85, 255, 133));
+
+       toInputAndDecryptFile();
+       isTakenAccess = true;
+
+      this.add(encrypt).setBounds(width-42,height-72,22,22);
+      this.add(setPassword).setBounds(width-72,height-72,22,22);
+    }
+    private void toInputAndDecryptFile() throws IOException {
+        FileInputStream inputStream = new FileInputStream("src//com//company//res//crypted.txt");
+        PrintStream output = new PrintStream(new FileOutputStream("src//com//company//res//decrypted.txt"));
+
+        Scanner sc = new Scanner(inputStream);
+
+        StringBuilder buffer = new StringBuilder();
+
+        String temp;
+        while (sc.hasNextLine()){
+            temp = sc.nextLine();
+            buffer.append(temp);
+            buffer.append("\n");
+        }
+
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < buffer.length(); i++) {
+            if (buffer.charAt(i) == '\n') {decryptedText.append('\n'); continue;}
+            decryptedText.append((char)((int) buffer.charAt(i) - 2));
+        }
+        output.print(decryptedText);
+
+        inputStream.close();
+        output.close();
+
+            try{
+                Process pr = Runtime.getRuntime().exec("cmd /c src\\com\\company\\res\\decrypted.txt");
+            }
+            catch(Exception e1){
+                System.out.println(e1.toString());
+                e1.printStackTrace();
+            }
+
+    }
+    private void toOutputAndEncryptFile() throws IOException {
+        FileInputStream inputStream = new FileInputStream("src//com//company//res//decrypted.txt");
+        PrintStream output = new PrintStream(new FileOutputStream("src//com//company//res//crypted.txt"));
+
+        Scanner sc = new Scanner(inputStream);
+
+        StringBuilder buffer = new StringBuilder();
+
+        String temp;
+        while (sc.hasNextLine()){
+            temp = sc.nextLine();
+            buffer.append(temp);
+            buffer.append("\n");
+        }
+
+        StringBuilder cryptedText = new StringBuilder();
+        for (int i = 0; i < buffer.length(); i++) {
+            if (buffer.charAt(i) == '\n') {cryptedText.append('\n'); continue;}
+            cryptedText.append((char)((int) buffer.charAt(i) + 2));
+        }
+        output.print(cryptedText);
+        inputStream.close();
+        output.close();
+    }
+    private boolean toCheckLicense() throws IOException {
+        if ((new File("src\\com\\company\\res\\License.txt")).exists()) {
+            FileInputStream in = new FileInputStream("src//com//company//res//License.txt");
+            Scanner sc = new Scanner(in);
+
+            String temp = sc.nextLine();
+
+            logo.setText("Licensed for "+temp.substring(10, temp.indexOf('.'))+" by Bulbum Lab, 2017");
+            this.correctLogin = temp.substring(10, temp.indexOf('.')).substring(0, temp.substring(10, temp.indexOf('.')).indexOf(' '));
+            repaint();
+
+            return true;
+        }
+         else {
+             license = new License();
+             license.setAlwaysOnTop(true);
+
+             license.licenseText.getDocument().addDocumentListener(new DocumentListener() {
+                @Override
+                public void insertUpdate(DocumentEvent e) {
+                    for (int i=0; i<license.arrayOfLicenses.length; i++)
+                       if (license.licenseText.getText().equals(license.arrayOfLicenses[i])){
+                           try {toPrintLicense(license.arrayOfUsernames[i]);
+                               username = license.arrayOfUsernames[i];
+
+                               logo.setText("Licensed for "+username+" by Bulbum Lab, 2017");
+                               correctLogin = username.substring(0,username.indexOf(' '));
+                               repaint();
+
+
+                           } catch (FileNotFoundException e1) {e1.printStackTrace();} catch (InterruptedException e1) {e1.printStackTrace();}
+                       }
+                }
+
+                @Override
+                public void removeUpdate(DocumentEvent e) {
+                    for (int i=0; i<license.arrayOfLicenses.length; i++)
+                        if (license.licenseText.getText().equals(license.arrayOfLicenses[i])){
+                            try {toPrintLicense(license.arrayOfUsernames[i]);
+                                username = license.arrayOfUsernames[i];
+                                logo.setText("Licensed for "+username+" by Bulbum Lab, 2017");
+                                repaint();
+
+                            } catch (FileNotFoundException e1) {e1.printStackTrace();} catch (InterruptedException e1) {e1.printStackTrace();}
+                        }
+                }
+
+                @Override
+                public void changedUpdate(DocumentEvent e) {
+
+                }
+            });
+             return false;
+             }
+    }
+    private void toPrintLicense(String user) throws FileNotFoundException, InterruptedException {
+        PrintStream output = new PrintStream(new FileOutputStream("src//com//company//res//License.txt"));
+        output.print("Username: "+user+".\nLicensed by Bulbum Lab, Minsk. 2017. All rights reserved\nCopying, illegal distribution of program fragments, source code, resources, and encryption algorithm is prosecuted in accordance with the legislation of the Russian Federation under the Federal Law \"On Trade Secrets\" of July 29, 2004 N 98-FZ.\nPersonal license: "+license.licenseText.getText());
+
+        license.toClose();
+        this.setVisible(true);
+    }
+    private void toChangePassword() throws IOException {
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+
+        try
+        {
+            String toWrite = toSubstitute(correctPassword);
+            fos = new FileOutputStream("src//com//company//res//config.bin");
+            oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(toWrite);
+            oos.flush();
+            oos.close();
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+    }
+    private void toReadPassword() throws IOException {
+        FileInputStream in = null;
+        ObjectInputStream ins = null;
+
+        try
+        {
+            in = new FileInputStream("src//com//company//res//config.bin");
+            ins = new ObjectInputStream(in);
+            correctPassword = (String) ins.readObject();
+            correctPassword = toSubstitute(correctPassword);
+
+            in.close();
+            ins.close();
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        catch (ClassNotFoundException e){e.printStackTrace();}
+    }
+    private String toSubstitute(String text){
+        StringBuilder substituted = new StringBuilder("");
+
+        for (int i=0; i<text.length(); i++)
+            switch (text.charAt(i)){
+                case ('a'): {substituted.append('z'); break;} //zyxwvutsrqponmlkjihgfedcba
+                case ('b'): {substituted.append('y'); break;} //
+                case ('c'): {substituted.append('x'); break;}
+                case ('d'): {substituted.append('w'); break;}
+                case ('e'): {substituted.append('v'); break;}
+                case ('f'): {substituted.append('u'); break;}
+                case ('g'): {substituted.append('t'); break;}
+                case ('h'): {substituted.append('s'); break;}
+                case ('i'): {substituted.append('r'); break;}
+                case ('j'): {substituted.append('q'); break;}
+                case ('k'): {substituted.append('p'); break;}
+                case ('l'): {substituted.append('o'); break;}
+                case ('m'): {substituted.append('n'); break;}
+                case ('n'): {substituted.append('m'); break;}
+                case ('o'): {substituted.append('l'); break;}
+                case ('p'): {substituted.append('k'); break;}
+                case ('q'): {substituted.append('j'); break;}
+                case ('r'): {substituted.append('i'); break;}
+                case ('s'): {substituted.append('h'); break;}
+                case ('t'): {substituted.append('g'); break;}
+                case ('u'): {substituted.append('f'); break;}
+                case ('v'): {substituted.append('e'); break;}
+                case ('w'): {substituted.append('d'); break;}
+                case ('x'): {substituted.append('c'); break;}
+                case ('y'): {substituted.append('b'); break;}
+                case ('z'): {substituted.append('a'); break;}
+
+                default: {substituted.append('-'); break;}
+            }
+            return substituted.toString();
+    }
+}
